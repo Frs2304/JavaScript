4691f80bf36d41a74a22a9b6018a38b635686326
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-17 01:19:43 +0530

    Word meaning dictionary

---
+++ Practice set 1 - Q5.js
@@ -0,0 +1,9 @@
+// Practixe set 1 - Q5
+// Creating a word meaning dictionary
+
+const dict = {
+    appreciate: "recognize the full worth of.",
+    ataraxia: "a state of freedom from emotional disturbance and anxiety.",
+    yakka: "work,especially hard work."
+}
+console.log(dict.appreciate);
