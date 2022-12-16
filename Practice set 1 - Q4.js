758170735e844804cbf05ac67f1a78ea9479890a
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-17 01:11:41 +0530

    Adding a new key to the const object

---
+++ Practice set 1 - Q4.js
@@ -0,0 +1,11 @@
+// Practise set 1 - Q4
+// Trying to add a new key to the const object
+
+const a1 = {
+    name : "Tushar",
+    section: 1,
+    isPrincipal: false
+}
+a1['friend'] = "Rohit";
+a1['Roll no'] = 23;
+console.log(a1);
