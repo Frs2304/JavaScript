ceac5c362cca50a5decb3b231ff932717d39be0c
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-16 22:13:53 +0530

    Update Primitive Datatypes.js

--- Primitive Datatypes.js
+++ Primitive Datatypes.js
@@ -1,8 +1,15 @@
+// Null datatype
 let a = null;
+// number datatype
 let b = 345;
+// boolean datatype
 let c = true;
+// BigInt datatype
 let d = BigInt("234") + BigInt("3");
+// String datatype
 let e = "Tushar";
+// Symbol datatype
 let f = Symbol("I am a nice symbol");
+// undefined datatype
 let g = undefined;
 console.log(a,b,c,d,e,f,g);
