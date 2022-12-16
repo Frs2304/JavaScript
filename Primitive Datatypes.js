1f6a7324c1f0c0a8bbc495624161fd562236ab22
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-16 22:07:24 +0530

    Primitive Datatypes in JavaScript

---
+++ Primitive Datatypes.js
@@ -0,0 +1,8 @@
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("234") + BigInt("3");
+let e = "Tushar";
+let f = Symbol("I am a nice symbol");
+let g = undefined;
+console.log(a,b,c,d,e,f,g);
