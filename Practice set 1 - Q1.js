b7abd651012d276efe278466020248f1785024d7
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-17 00:10:33 +0530

    Update Practice set 1 - Q1.js

--- Practice set 1 - Q1.js
+++ Practice set 1 - Q1.js
@@ -1,5 +1,5 @@
 // Chapter 1 Q1
-// Creating a avraible of type string and try to add a number to it
+// Creating a variable of type string and try to add a number to it
 let a = "Frs";
 let b = 23;
 console.log(a+b);
