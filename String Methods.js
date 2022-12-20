189bed785f90120a420f2db74543e6305d6c0a6a
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-21 02:03:31 +0530

    Update String Methods.js

--- String Methods.js
+++ String Methods.js
@@ -10,3 +10,9 @@ console.log(n.toLowerCase());
 console.log(n.slice(2,4));
 // replaces the string characters
 console.log(n.replace("Tushar" , "Yash"));
+let friend = "Tushar";
+let friend2 = "Yash";
+// to add two string
+console.log(friend.concat(" is a friend of ", friend2));
+// prints string without spaces
+console.log(friend.trim());
