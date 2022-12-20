06cbbed4639c004d9de7ab0c0e4ded4df965d98b
Tushar Mishra <you@example.com>
2022-12-21 01:50:00 +0530

    String Methods in JavaScript

---
+++ String Methods.js
@@ -0,0 +1,12 @@
+let n = "Tushar";
+console.log(n);
+// prints the length of the string
+console.log(n.length)
+// prints the string in Uppercase characters
+console.log(n.toUpperCase());
+// prints the string in Lowercase characters
+console.log(n.toLowerCase());
+// returns the character from index 2 till 4 (not includes 4)
+console.log(n.slice(2,4));
+// replaces the string characters
+console.log(n.replace("Tushar" , "Yash"));
