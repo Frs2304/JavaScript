81b784934c22260f99814c2ec7b0196d4f697eb8
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-23 18:13:12 +0530

    More Array methods

---
+++ Array Methods 2.js
@@ -0,0 +1,11 @@
+let num = [1,2,3,4,5,6,7,8,9]
+delete num[0]
+console.log(num) 
+let num_2 = [11,12,13,14,15,16,17,18,19]
+// concat method is used to merge two arrays
+let newArray = num.concat(num_2)
+console.log(newArray)
+// sort method
+let num_3 = [5,4,1,8,7]
+num_3.sort()
+console.log(num_3)
