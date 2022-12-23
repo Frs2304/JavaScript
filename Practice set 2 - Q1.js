7df303c4f5243c3dcb986b128fe62aa7508292f2
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-23 13:58:58 +0530

    Practice set 2

---
+++ Practice set 2 - Q1.js
@@ -0,0 +1,8 @@
+// Practice set 2 - Q1
+let age = prompt("What is your age?")
+if(age>10 && age<20){
+    console.log("Your age lies between 10 and 20")
+}
+else{
+    console.log("Your age doesn't lies between 10 and 20")
+}
