c9084a05f62dc2b57ecc0efb3226389dbf2fd8da
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-24 22:23:17 +0530

    Using Loops in Array

---
+++ Loops with Array.js
@@ -0,0 +1,9 @@
+let num = [3,23,1,2,4]
+for(let i=0;i<num.length;i++){
+    console.log(num[i])
+}
+// for each loop
+num.forEach((element)=>{
+    // to print square of array elements
+    console.log(element*element)
+})
