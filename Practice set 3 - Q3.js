3a8305af283d59c8d34e43e2bb71ca5008133786
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-28 17:18:18 +0530

    Practice Set 3 - Q3

---
+++ Practice set 3 - Q3.js
@@ -0,0 +1,9 @@
+// Practice Set 3 - Q3
+// Write a program to print "Try Again!" until teh user enters the correct number.
+let cn = 23
+let i 
+while(i != cn){
+    console.log("Try Again!")
+    i = prompt("Enter a number")
+}
+console.log("You have entered a correct number")
