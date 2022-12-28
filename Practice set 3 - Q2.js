fc5a93d87cd0895dadd8b78ef7c0db4c871600c2
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-28 16:48:20 +0530

    Practice Set 3 - Q2

---
+++ Practice set 3 - Q2.js
@@ -0,0 +1,11 @@
+// Practice Set 3 - Q2
+// Same problem as Q1 but using for in loop here
+let marks = {
+    Tushar: 95,
+    Yash: 90,
+    Harry: 47,
+    Lovish: 88
+}
+for(let i in marks){
+    console.log("The marks of " + i + " are " + marks[i])
+}
