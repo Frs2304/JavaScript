ce7778a9cfa478668b6bb4a437f787b118b08a4a
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-29 23:49:49 +0530

    Update Practice set 4 - Q2.js

--- Practice set 4 - Q2.js
+++ Practice set 4 - Q2.js
@@ -3,4 +3,4 @@
 const sentence = 'The quick brown fox jumps over the lazy dog';
 const word = 'fox';
 console.log(sentence.includes(word));
-console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`)
+console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`);
