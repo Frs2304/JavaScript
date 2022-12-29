c82b4ebfd6ad4ae3d0b7adef1eb1b10cb6e165c1
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-30 00:29:44 +0530

    Practice set 4 - Q5

---
+++ Practice set 4 - Q5.js
@@ -0,0 +1,6 @@
+// Practice Set 4 - Q5
+// Change the 4th character of the string
+let friend = "Yash"
+friend[3] = 'r'
+console.log(friend)
+// Output : We cannot change the 4th character of the string as strings are immutableS
