56da3d80ffd7932b8c0a6c7cdd728b97dca0ee1e
Tushar Mishra <tusharmishra2304@gmail.com>
2023-01-02 23:40:25 +0530

    Practice set 5 - Q2

---
+++ Practice set 5 - Q2.js
@@ -0,0 +1,10 @@
+// Practice Set 5 - Q2
+let arr = [1,2,3,4,5,6,7]
+let a;
+do {
+ a = prompt("Enter a number")
+a = Number.parseInt(a)
+arr.push(a)
+console.log(arr)
+}while(a!=0)
+console.log(arr)
