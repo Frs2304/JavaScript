471119967ea44e683ebce9a170111bd6267b89dc
Tushar Mishra <tusharmishra2304@gmail.com>
2023-01-02 23:16:36 +0530

    Update Practice set 5 - Q1.js

--- Practice set 5 - Q1.js
+++ Practice set 5 - Q1.js
@@ -1,5 +1,5 @@
 // Practice Set 5 - Q1
-// Create an arrat of numbers and take input from the user to add numbers yo this array
+// Create an array of numbers and take input from the user to add numbers yo this array
 let arr = [1,2,3,4,5,6,7]
 let a = prompt("Enter a number")
 a = Number.parseInt(a)
