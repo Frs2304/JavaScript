a694a6c632968c5dd1c240948b2cf85e874a3d31
Tushar Mishra <tusharmishra2304@gmail.com>
2023-01-03 23:34:28 +0530

    Prompt Function

---
+++ Practice set 6 - Q1.js
@@ -0,0 +1,10 @@
+// Practice Set 6 - Q1
+
+let age = prompt("Enter your age : ")
+let n = Number.parseInt(age)
+if(n<18){
+    console.log("You cannot drive!")
+}
+else{
+    console.log("You can drive :)")
+}
