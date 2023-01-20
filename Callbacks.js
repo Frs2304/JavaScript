f82d6d14259697e68f961da5c38e70ab6c48c0a7
Tushar Mishra <tusharmishra2304@gmail.com>
2023-01-20 23:28:16 +0530

    Callbacks function in JavaScript

---
+++ Callbacks.js
@@ -0,0 +1,10 @@
+// Callbacks
+function loadScript(src){
+    var script = document.createElement("script")
+    script.src = src;
+    script.onload = function(){
+        console.log("SRC: " + src())
+    }
+    document.body.appendChild(script);
+}
+loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js")
