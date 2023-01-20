1f62182f50e8f6127bfc68cdcebe4ed2df0c091e
Tushar Mishra <tusharmishra2304@gmail.com>
2023-01-21 00:27:35 +0530

    Error Handling in JavaScript

---
+++ Error Handling.js
@@ -0,0 +1,18 @@
+setTimeout(()=>{
+  console.log("Hacking wifi....Please wait....")
+},1000)
+try{
+  console.log(yash)
+}
+catch(error){
+  console.log(error)
+}
+setTimeout(()=>{
+  console.log("Fetching username and password....Please wait....")
+},2000)
+setTimeout(()=>{
+  console.log("Hacking Yash's Instagram id....Please wait....")
+},3000)
+setTimeout(()=>{
+  console.log("Username and password of Yash fetched....Please wait....")
+},4000)
