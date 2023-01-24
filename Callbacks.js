5ab29e87075171487fcb4b2de399b3095ceafbbf
Tushar Mishra <tusharmishra2304@gmail.com>
2023-01-24 23:09:50 +0530

    Callbacks.js

--- Callbacks.js
+++ Callbacks.js
@@ -7,4 +7,4 @@ function loadScript(src){
     }
     document.body.appendChild(script);
 }
-loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js")
+loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js",script)
