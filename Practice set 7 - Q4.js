e054aa61306816cc6f91dfb09eaa1cb48f38c2de
Tushar Mishra <tusharmishra2304@gmail.com>
2023-01-26 23:55:05 +0530

    Update Practice set 7 - Q4.js

--- Practice set 7 - Q4.js
+++ Practice set 7 - Q4.js
@@ -1,6 +1,5 @@
 // Practice Set 7 - Q4
 // JavaScript code to change background of all <li> tag to cyan
-
 array.from(document.getElementsbyTagName("li")).forEach((element) =>{
     element.style.background = "cyan";
 })
