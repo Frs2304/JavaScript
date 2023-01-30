b6055515f12119d59d4d44cb91aa6e1e5c4a381e
Tushar Mishra <tusharmishra2304@gmail.com>
2023-01-30 22:29:14 +0530

    Classes in JavaScript

---
+++ Classes in JavaScript.js
@@ -0,0 +1,15 @@
+class RailwayForm {
+    submit() {
+        alert("form submitted")
+    }
+    cancel(){
+        alert("This form is cancelled")
+    }
+}
+
+let tushar = new RailwayForm()
+let yash = new RailwayForm()
+
+tushar.submit()
+yash.submit()
+yash.cancel()
