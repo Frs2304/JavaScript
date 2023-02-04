9c8a31278eff0340e1bf0d3110ac0c7862b9156a
Tushar Mishra <tusharmishra2304@gmail.com>
2023-02-04 20:57:41 +0530

    Method Overloading

---
+++ Method Overloading.js
@@ -0,0 +1,25 @@
+class Employee {
+    login(){
+        console.log("Employee has logged in")
+    }
+    logout(){
+        console.log("Employee has logged out")
+    }
+    requestLeaves(leaves){
+        console.log(`Employee has requested ${leaves} leaves`)
+    }
+}
+class Programmer extends Employee {
+    requestCoffee(x){
+        console.log(`Employee has requested ${x} coffee`)
+    }
+    requestLeaves(leaves){
+        super.requestLeaves(5)
+            console.log("One extra is granted")
+    }
+}
+let e = new Programmer()
+e.login()
+e.logout()
+e.requestCoffee(5)
+e.requestLeaves(6)
