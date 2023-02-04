a8b285b97a7ee72b4bdf157d0a889a390f3f6c57
Tushar Mishra <tusharmishra2304@gmail.com>
2023-02-04 20:57:15 +0530

    Constructor Overloading

---
+++ Constructor Overloading.js
@@ -0,0 +1,33 @@
+class Employee {
+    constructor(name) {
+        console.log(`Employee's constructor is here`);
+        this.name = name
+    }
+    login() {
+        console.log("Employee has logged in");
+    }
+    logout() {
+        console.log("Employee has logged out");
+    }
+    requestLeaves(leaves) {
+        console.log(`Employee has requested ${leaves} leaves`);
+    }
+}
+class Programmer extends Employee {
+    constructor(name){
+        super(name)
+        console.log(`This is newly written constructor`)
+    }
+    requestCoffee(x) {
+        console.log(`Employee has requested ${x} coffee`);
+    }
+    requestLeaves(leaves) {
+        super.requestLeaves(5)
+        console.log("One extra is granted");
+    }
+}
+let e = new Programmer();
+e.login();
+e.logout();
+e.requestCoffee(5);
+e.requestLeaves(6);
