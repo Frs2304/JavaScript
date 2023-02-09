073763bb3adea96f8f81d2eff1d884bee8907d9b
Tushar Mishra <tusharmishra2304@gmail.com>
2023-02-09 23:23:58 +0530

    Update Error Handling.js

--- Error Handling.js
+++ Error Handling.js
@@ -1,18 +1,18 @@
 setTimeout(()=>{
   console.log("Hacking wifi....Please wait....")
-},1000)
+},1000);
 try{
-  console.log(yash)
+  console.log(yash);
 }
 catch(error){
-  console.log(error)
+  console.log(error);
 }
 setTimeout(()=>{
   console.log("Fetching username and password....Please wait....")
-},2000)
+},2000);
 setTimeout(()=>{
   console.log("Hacking Yash's Instagram id....Please wait....")
-},3000)
+},3000);
 setTimeout(()=>{
   console.log("Username and password of Yash fetched....Please wait....")
-},4000)
+},4000);
