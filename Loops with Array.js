d805788cde46753afdaa6adc5e6e04ce2149211d
Tushar Mishra <tusharmishra2304@gmail.com>
2023-03-20 23:46:52 +0530

    Update Loops with Array.js

--- Loops with Array.js
+++ Loops with Array.js
@@ -1,21 +1,21 @@
-let num = [3,23,1,2,4]
+let num = [3,23,1,2,4];
 for(let i=0;i<num.length;i++){
-    console.log(num[i])
+    console.log(num[i]);
 }
 // for each loop
 num.forEach((element)=>{
     // to print square of array elements
-    console.log(element*element)
+    console.log(element*element);
 })
 // .from is used to create an array from any object
-let name = "Tushar"
-let arr = Array.from(name)
-console.log(arr)
+let name = "Tushar";
+let arr = Array.from(name);
+console.log(arr);
 // for of loop
 for(let i of num){
-    console.log(i)
+    console.log(i);
 }
 // for in loop (used to print index of i)
 for(let i in num){
-    console.log(i)
+    console.log(i);
 }
